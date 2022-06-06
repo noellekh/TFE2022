62c5a1201d53cd636c459f431ce1503a582c26a1
noellekh <63249943+noellekh@users.noreply.github.com>
2022-06-06 20:48:13 +0200

    agenda client frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,29 @@
-import
+import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom"
+import Accueil from './pages/RdvClients';
+import RdvClients from './pages/RdvClients';
+import Trainings from './pages/Trainings';
+
+
+function App() {
+
+  
+  return (
+    <div className="App"> 
+    <Router>
+      <Link to ="/">Accueil</Link>
+      <Link to ="/rdv">Réserver</Link>
+      <Link to ="/trainings">Trainings</Link>
+      <Routes>
+        <Route path = "/" element={<Accueil/>}/>
+        <Route path = "/rdv" element = {<RdvClients/>}/>
+        <Route path = "/trainings" element ={<Trainings/>}/>
+      </Routes>
+    </Router>
+
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ client/src/pages/RdvClients.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function RdvClients() {
+    const [listAgendaClient, setListAgendaClient] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3001/agendaclient").then ((response)=>{
+            setListAgendaClient(response.data)
+      
+    })
+
+  }, [])
+  return (
+    <div>  <h1>Hey</h1>
+    {listAgendaClient.map((value, key)=>{ 
+            return (
+                <div>
+                    <h1>Hello</h1>
+                    <h2>{value.ag_date}</h2>
+                </div>
+            );
+            })}
+    </div>
+   
+  )
+}
+
+export default RdvClients
