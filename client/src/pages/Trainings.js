859595f498bc82ceda758712e6306e3bd3610124
noellekh <63249943+noellekh@users.noreply.github.com>
2022-06-06 20:48:58 +0200

    trainings frontend

---
+++ client/src/pages/Trainings.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+
+function Trainings() {
+    const initialValues={
+        score: ""
+    };
+
+    const onSubmit = (data) =>{
+        // faire la db d'abord !
+        //axios.post("http://localhost:3001/agendaclient", data).then ((response)=>{
+        console.log('yes !')
+       // });
+    };
+
+    const validationSchema = Yup.object().shape({
+        score: Yup.string()
+    })
+
+
+  return (
+    <div className='trainings'>
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+            <Form>
+                <label>Entrez votre score: </label>
+                <ErrorMessage name='score' component="span"/>
+                <Field id="inputCreateScore" name="score"/>
+                <button type='submit'>Ajouter score</button>
+            </Form>
+
+        </Formik>
+
+    </div>
+  )
+}
+
+export default Trainings
