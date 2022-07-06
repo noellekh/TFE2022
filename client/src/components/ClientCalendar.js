684b27ed305551eaa92123e9e79795fb9b43e602
noellekh <63249943+noellekh@users.noreply.github.com>
2022-07-06 20:20:53 +0200

    sauv DB ok

--- client/src/components/ClientCalendar.js
+++ client/src/components/ClientCalendar.js
@@ -5,10 +5,9 @@ import moment from "moment";
 import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 import { useParams } from "react-router-dom";
-
-
-//import format from 'date-fns'
-
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+import getDay from "date-fns/getDay"
 import { registerLocale } from "react-datepicker";
 import fr from 'date-fns/locale/fr';
 import { useEffect } from "react";
@@ -19,13 +18,60 @@ registerLocale('fr', fr)
 function ClientCalendar() {
     let {user_id} = useParams();
 
-    const [chooseDate, setChooseDate] = useState(null);
+    const [chooseDate, setChooseDate] = useState(null)
     const [coaching, setCoaching] = useState('')
-    const navigate = useNavigate()
+    const [chosenDate, setChosenDate]= useState([]);
+    const [excludeDate, setExcludeDate]= useState([])
+    const navigate = useNavigate();
+
+    const exclu = excludeDate.map((val)=>{
+        return val.ag_date
+    });
+
+    console.log("dates prise: ", exclu)
+    console.log("date choisie", chooseDate)
+
+    const PassedTime = (time) => {
+        const currentDate = new Date();
+        const selectedDate = new Date(time);
+
+        return currentDate.getTime() < selectedDate.getTime();
+    };
+
+    const isAvailable = () => {
+        const notAvailday = excludeDate.map((val)=>{
+            return val.ag_date
+        });
+
+        for (var i=0; i < notAvailday.length; i++){
+            //console.log('not available: ', notAvailday[i])
+            return notAvailday[i]
+        }
+        
+        console.log(notAvailday, "")
+
+        
+    }
+
+           
+
+
+    const chooseDateFormat = moment(chooseDate).format("DD/MM/YYYY")
+
+    for (var i=0; i < exclu.length; i++){
+        const excludeDateFormat = moment(exclu[i]).format("DD/MM/YYYY")
+        //console.log("DATES ",excludeDateFormat)
+    }
+    
+
+    //console.log(typeof(exclu), exclu[1], exclu.length)
+    //console.log(chooseDateFormat)
+    //console.log(typeof(chooseDate),chooseDate)
+
 
     const handleChoiceDate=(date)=>{
+
         setChooseDate(date)
-        console.log(chooseDate)
         return date;
     };
 
@@ -37,6 +83,7 @@ function ClientCalendar() {
             headers: {
                 'Content-Type': 'application/json',
                 "Access-Control-Allow-Origin": "*",
+                accessToken: localStorage.getItem("accessToken"),
                 }
             }).then(()=>{
                 console.log('ok')
@@ -51,6 +98,17 @@ function ClientCalendar() {
 
     }
 
+    useEffect(()=>{
+        axios.get(`http://localhost:3001/agendaclient/${user_id}`).then((response)=>{
+            setChosenDate(response.data)
+            
+        });
+
+        axios.get("http://localhost:3001/agendaclient/").then((response)=>{
+            setExcludeDate(response.data)
+        })
+    },[])
+
   return (
 <div className="ag-client-datepicker">
             <div>
@@ -63,17 +121,31 @@ function ClientCalendar() {
                     <DatePicker
                         selected={chooseDate}
                         onChange={handleChoiceDate}
-                        dateFormat="yyyy/dd/MM EE HH:mm "
+                        minDate= {new Date()}
+                        filterDate={isAvailable}
                         showTimeSelect
                         timeintervals={60}
-                        locale='fr'
+                        filterTime={PassedTime}
+                        minTime={setHours(setMinutes(new Date(), 0), 9)}
+                        maxTime={setHours(setMinutes(new Date(), 0), 17)}
+                        locale={fr}
                     />
                     <button onClick={storeDate} className="button-calendar">Sauvegarder</button>
 
                     <div className="ag-client-choix">
-                        <p>Vous avez choisi {chooseDate ? chooseDate.toString(): null}</p>
-                        <p>{moment(chooseDate).format("dddd  DD/MM/yyyy à HH:mm")}</p>
+                        <p>Vous avez choisi {chooseDateFormat ? chooseDateFormat.toString(): null}</p>
+                        <p>{moment(chooseDate).format("dddd DD/MM/yyyy à HH:mm")}</p>
+                    </div>
 
+                    <div className="ag-client-rdv">
+                        <h2>Vos rendez-vous</h2>
+                        {chosenDate.map((choose, key)=>{
+                            return(
+                                <div className="my-coaching" key={key}>
+                                    {choose.ag_date}
+                                </div>
+                            )
+                        })}
                     </div>
 
                 </form>
